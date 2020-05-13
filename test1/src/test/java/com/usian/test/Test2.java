8ccf75717d367f32bf4b43ed761f9d2a8c2c3ff4
1279232215 <1279232215@qq.com>
2020-05-13 15:17:28 +0800

    15:17修改了Test2

--- test1/src/test/java/com/usian/test/Test2.java
+++ test1/src/test/java/com/usian/test/Test2.java
@@ -1,7 +1,7 @@
 package com.usian.test;
 
 public class Test2 {
-    //三生三世 : 十里桃花
+    //三生三世 : 十里桃花 他就像是一道疤
     public static void main(String[] args) {
         System.out.println("222");
     }
